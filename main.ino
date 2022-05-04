67a0c438083600cdd32fb0a11d6bd031b98d0666
Mufasa A <40151627+mufasa159@users.noreply.github.com>
2022-05-04 16:26:56 -0400

    update constraints

--- main.ino
+++ main.ino
@@ -14,7 +14,7 @@ float moisture;
 
 float MAX_MOIST = 300.00;    // Higher ? soil is dry
 float MIN_HUMIDITY = 45.00;  // Lower  ? lack of water on air
-
+float TOO_DRY = 450.00;      // Higher ? too dry to consider humidity 
 int water_pump = 6;          // D6 Pin
 
 void setup() {
@@ -30,7 +30,12 @@ void loop() {
   
   if (moisture > MAX_MOIST && humidity < MIN_HUMIDITY) {
     digitalWrite(water_pump, HIGH);
-    delay(5000);
+    delay(10000);
+    digitalWrite(water_pump, LOW);
+    delay(10000);
+  } else if (moisture > TOO_DRY) {
+    digitalWrite(water_pump, HIGH);
+    delay(15000);
     digitalWrite(water_pump, LOW);
     delay(10000);
   }
