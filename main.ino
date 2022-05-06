46cd3378745cc2772be4de547d948bd89041be8e
Mufasa A <40151627+mufasa159@users.noreply.github.com>
2022-05-06 14:19:09 -0400

    handler for moist sensor disconnection

--- main.ino
+++ main.ino
@@ -12,9 +12,10 @@ float humidity;
 float temperature;
 float moisture;
 
-float MAX_MOIST = 300.00;    // Higher ? soil is dry
+int DISCONNECTED = 800;      // Higher ? sensor possibly disconnected
+int MAX_MOIST = 300;         // Higher ? soil is dry
+int TOO_DRY = 450;           // Higher ? too dry to consider humidity 
 float MIN_HUMIDITY = 45.00;  // Lower  ? lack of water on air
-float TOO_DRY = 450.00;      // Higher ? too dry to consider humidity 
 int water_pump = 6;          // D6 Pin
 
 void setup() {
@@ -23,19 +24,29 @@ void setup() {
   pinMode(water_pump, OUTPUT);
 }
 
+// To include:
+// 1. Water level monitoring
+// 2. Motor shut off
+// 3. LED indicators
+
 void loop() {
   moisture = analogRead(A1);
   humidity = dht.readHumidity();
   temperature = (dht.readTemperature() * 1.8) + 32;
   
-  if (moisture > MAX_MOIST && humidity < MIN_HUMIDITY) {
+  if (moisture > DISCONNECTED) {
+    digitalWrite(water_pump, LOW);
+    Serial.println("Moist Sensor Disonnected");
+    
+  } else if (moisture > MAX_MOIST && humidity < MIN_HUMIDITY) {
     digitalWrite(water_pump, HIGH);
-    delay(10000);
+    delay(5000);
     digitalWrite(water_pump, LOW);
     delay(10000);
+    
   } else if (moisture > TOO_DRY) {
     digitalWrite(water_pump, HIGH);
-    delay(15000);
+    delay(7000);
     digitalWrite(water_pump, LOW);
     delay(10000);
   }
